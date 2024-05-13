b5a3c1af515d7bfea9dc068b23605e61bf5fde75
wxz <3337876732@qq.com>
2024-05-13 19:01:00 +0800

    dom4j读取配置

--- generator/src/main/java/com/wxz/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/wxz/train/generator/server/ServerGenerator.java
@@ -2,6 +2,7 @@ package com.wxz.train.generator.server;
 
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -31,8 +32,32 @@ public class ServerGenerator {
      * @throws Exception 可能抛出的异常，包括TemplateException和其他IO异常。
      * @throws TemplateException 当处理模板时发生错误。
      */
-    public static void main(String[] args) throws Exception, TemplateException {
+    public static void main(String[] args) throws Exception {
+        // 获取生成器路径
+        String generatorPath = getGeneratorPath();
 
+        // 使用SAXReader读取并解析XML文件
+        Document document = new SAXReader().read("generator/" + generatorPath);
+
+        // 定位到table节点
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+
+        // 获取table节点的tableName和domainObjectName属性值
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+
+        // 输出属性值
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+    }
+
+    /**
+     * 获取生成器路径。
+     * 该方法通过读取指定的XML文件，解析出其中的configurationFile节点的文本内容作为生成器路径。
+     * @return 生成器路径字符串。
+     * @throws DocumentException 如果读取或解析XML文件发生错误。
+     */
+    private static String getGeneratorPath() throws DocumentException {
         // 创建SAXReader实例用于读取XML文档
         SAXReader saxReader = new SAXReader();
         // 定义命名空间的映射
@@ -53,14 +78,7 @@ public class ServerGenerator {
         // 输出选择节点的文本内容
         System.out.println(node.getText());
 
-        /*// 此代码块被注释，原功能是初始化FreeMarker模板配置并使用模板生成Java文件。
-        // 初始化FreeMarker模板配置
-        FreeMakerUtil.initConfig("test.ftl");
-        // 准备模板参数，例如这里将"domain"设置为"Test1"
-        Map<String, Object> param = new HashMap<>();
-        param.put("domain", "Test1");
-        // 使用模板和参数生成"Test1.java"文件
-        FreeMakerUtil.generator(toPath + "Test1.java", param);*/
+        // 返回节点的文本内容
+        return node.getText();
     }
-
 }
