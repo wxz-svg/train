6167e7481cde8c7d2f2d17e63b4a7a49ecf49f50
wxz <3337876732@qq.com>
2024-05-15 23:11:36 +0800

    集成quartz

---
+++ batch/src/main/java/com/wxz/train/batch/job/TestJob.java
@@ -0,0 +1,13 @@
+package com.wxz.train.batch.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("TestJob TEST");
+    }
+}
