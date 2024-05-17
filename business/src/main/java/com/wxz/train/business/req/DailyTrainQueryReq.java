a5820a95b0e46c59b02fe015de65ac568a24a8bf
wxz <3337876732@qq.com>
2024-05-17 08:49:26 +0800

    每日车次页面增加日期和车次查询条件

--- business/src/main/java/com/wxz/train/business/req/DailyTrainQueryReq.java
+++ business/src/main/java/com/wxz/train/business/req/DailyTrainQueryReq.java
@@ -2,8 +2,15 @@ package com.wxz.train.business.req;
 
 import com.wxz.train.common.req.PageReq;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 public class DailyTrainQueryReq extends PageReq {
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String code;
 }
